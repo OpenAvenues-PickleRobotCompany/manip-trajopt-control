c7c02d8797b430aead9be7bb0939a27a4f2b5ab8
Noe Fontana <noe.fontana@grenoble-inp.org>
2022-09-07 21:14:23 -0400

    wip auto formatter not working on vscode

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main() -> None:
+    t = 'thisiiiiiiiiiiiaaaaaaaa' + 'thisiiiiiiiiiiiaaaaaaaa' + 'thisiiiiiiiiiiiaaaaaaaa' +'thisiiiiiiiiiiiaaaaaaaa' + 'thisiiiiiiiiiiiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii'
+
+
+
+
+
+if __name__ == '__main__':
+    main()
